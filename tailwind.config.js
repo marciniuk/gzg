ddc422c63962cbc8d5471367367a60a92d02a577
Adrian Marciniuk <marciniuk.adrian123@gmail.com>
2020-06-26 00:21:22 +0200

    casters

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,28 @@
 // tailwind.config.js
-const { colors } = require('tailwindcss/defaultTheme')
+const { colors } = require("tailwindcss/defaultTheme");
 
 module.exports = {
   theme: {
     listStyleType: {
-      none: 'none',
-      disc: 'disc',
-      decimal: 'decimal',
-      square: 'square',
-      'upper-roman': 'upper-roman',
-      'lower-roman': 'lower-roman',
-      'upper-alpha': 'upper-alpha',
-      'lower-alpha': 'lower-alpha',
+      none: "none",
+      disc: "disc",
+      decimal: "decimal",
+      square: "square",
+      "upper-roman": "upper-roman",
+      "lower-roman": "lower-roman",
+      "upper-alpha": "upper-alpha",
+      "lower-alpha": "lower-alpha",
     },
     extend: {
       colors: {
         gray: {
           ...colors.gray,
-          '400': '#cccccc',
-          '700': '#22262e',
-          '800': '#1e2129',
-          '900': '#181b21',
-        }
-      }
-    }
-  }
-}
+          "400": "#cccccc",
+          "700": "#22262e",
+          "800": "#1e2129",
+          "900": "#181b21",
+        },
+      },
+    },
+  },
+};
