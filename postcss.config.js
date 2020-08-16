d3e4b7d194557574a17a3c833a213c5fc59101fb
Adrian Marciniuk <marciniuk.adrian123@gmail.com>
2020-08-16 16:26:42 +0200

    gracz + update

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,3 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./_site/**/*.html", "./assets/js/main.js"],
-  defaultExtractor: (content) => {
-    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
-
-    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
-
-    return broadMatches.concat(innerMatches);
-  },
-});
-
 module.exports = {
-  plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-  ],
+  plugins: [require("tailwindcss"), require("autoprefixer")],
 };
