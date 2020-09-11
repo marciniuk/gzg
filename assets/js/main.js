2e94c2b21eb06ab3e2c9bfd153dc06308132c1ea
Adrian Marciniuk <marciniuk.adrian123@gmail.com>
2020-09-11 16:01:14 +0200

    poprawa polskiej wersji

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,11 +51,11 @@ if (window.location.pathname === "/pl/") {
     let minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((t % (1000 * 60)) / (1000));
     document.querySelector("#GZGPL").innerHTML =
-      "in " + days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+      "za " + days + "d " + hours + "h " + minutes + "m " + seconds + "s";
     if (t < 0) {
       clearInterval(x);
       document.querySelector("#GZGPL").innerHTML =
-        'NOW!';
+        'TERAZ!';
     }
   }, 1000);
 }
